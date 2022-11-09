63d7419040b5be69e35ec363ac21d216ca72dbb8
circles-png <matthew.li10@education.nsw.gov.au>
2022-11-09 19:49:30 +1100

    chrome fix

--- src/ts/settings.ts
+++ src/ts/settings.ts
@@ -2,12 +2,15 @@ const themeSwitch = document.getElementById("themeSwitch") as HTMLInputElement;
 
 themeSwitch.onchange = (event) => {
     let target = event.target as HTMLInputElement;
-    chrome.storage.sync.set({ baulkoTheme: target.checked }, () => {
-        console.log(`Successfully written baulkoTheme for Chrome: ${themeSwitch.checked}`)
-    })
-    browser.storage.sync.set({ baulkoTheme: target.checked }).then(() => {
-        console.log(`Successfully written baulkoTheme for Firefox: ${themeSwitch.checked}`)
-    })
+    if (isFirefox) {
+        browser.storage.sync.set({ baulkoTheme: target.checked }).then(() => {
+            console.log(`Successfully written baulkoTheme for Firefox: ${themeSwitch.checked}`)
+        })
+    } else {
+        chrome.storage.sync.set({ baulkoTheme: target.checked }, () => {
+            console.log(`Successfully written baulkoTheme for Chrome: ${themeSwitch.checked}`)
+        })
+    }
 
     updateTheme()
 }
--- src/ts/theme.ts
+++ src/ts/theme.ts
@@ -1,20 +1,21 @@
+// @ts-ignore
+const isFirefox = typeof InstallTrigger !== 'undefined';
+
 const getThemeFromStorage = async (): Promise<boolean> => {
-    let themes = [
-        await chrome.storage.sync.get("baulkoTheme")
-            .then((items: { [baulkoTheme: string]: boolean }): boolean => {
-                let theme = items.baulkoTheme
-                console.log(`Successfully read baulkoTheme for Chrome: ${theme}`)
-                return theme
-            }),
-        await browser.storage.sync.get("baulkoTheme")
+    return isFirefox
+        ? await browser.storage.sync.get("baulkoTheme")
             .then((items: { [baulkoTheme: string]: browser.storage.StorageValue }): boolean => {
                 let theme = items.baulkoTheme as boolean
                 console.log(`Successfully read baulkoTheme for Firefox: ${theme}`)
                 return theme
-            }),
-    ]
-    if (!(themes.every((theme) => theme === themes[0]))) console.error("Themes in storage do not match!")
-    return themes[0]
+            })
+        : await chrome.storage.sync.get("baulkoTheme")
+            .then((items: { [baulkoTheme: string]: boolean }): boolean => {
+                let theme = items.baulkoTheme
+                console.log(`Successfully read baulkoTheme for Chrome: ${theme}`)
+                return theme
+            })
+
 }
 
 const updateTheme = (): void => {
@@ -22,9 +23,6 @@ const updateTheme = (): void => {
         .then((dark) => {
             if (dark) document.body.parentElement?.classList.add("dark")
             else document.body.parentElement?.classList.remove("dark")
-        })
-        .then(() => {
-            document.body.classList.add("transition-colors", "duration-300")
         })
 }
 
